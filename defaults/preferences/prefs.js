0e6f5ff476fcd2618c5231450a34bf0556a3f6f3
Yin QIU <qiuyin@gmail.com>
2010-07-04 23:31:18 +0800

    initial work of v1.1

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -2,5 +2,11 @@ pref("extensions.tabby.boolpref", false);
 pref("extensions.tabby.intpref", 0);
 pref("extensions.tabby.stringpref", "A string");
 
+// the maximum number of result shown in the list. -1 for unlimited results
+pref("extensions.tabby.maxResults", 12);
+// whether or not to turn on fuzzy match
+pref("extensions.tabby.fuzzy", false);
+
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
 pref("extensions.tabby@qiuyin.name.description", "chrome://tabby/locale/overlay.properties");
+
